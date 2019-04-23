7c52e63af63f7ebae6d07cb26e46a573851eb0c9
unknown <rmtestard@gmail.com>
2019-04-23 16:09:43 +0200

    nested queries

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,12 +8,31 @@ const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 //   { id: '23', firstName: 'Samantha', age: 21 }
 // ];
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // console.log(parentValue, args);
+        // console.log(parentValue.companyId);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
   }
 });
 
