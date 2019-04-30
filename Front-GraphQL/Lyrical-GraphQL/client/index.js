2aac0deb9c962fd6bd0981aaab2376017cd57e73
unknown <rmtestard@gmail.com>
2019-04-30 10:29:42 +0200

    querying data on the client

--- Front-GraphQL/Lyrical-GraphQL/client/index.js
+++ Front-GraphQL/Lyrical-GraphQL/client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
