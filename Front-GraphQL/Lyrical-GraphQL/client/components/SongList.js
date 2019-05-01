cd4ef6be3c4b00e9f94c0561987c552197fa9530
unknown <rmtestard@gmail.com>
2019-05-01 11:06:32 +0200

    styling

--- Front-GraphQL/Lyrical-GraphQL/client/components/SongList.js
+++ Front-GraphQL/Lyrical-GraphQL/client/components/SongList.js
@@ -5,7 +5,9 @@ import { graphql } from 'react-apollo';
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => (
-      <li key={song.id}>{song.title}</li>
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>
     ));
   }
 
@@ -14,7 +16,7 @@ class SongList extends Component {
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
-    return <div>{this.renderSongs()}</div>;
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
